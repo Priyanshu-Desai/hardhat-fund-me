3f4d129932082b7c5b09d7d6d0919307af8f8112
Priyanshu Desai <desai09p@gmail.com>
2026-01-19 20:01:18 +0000

    refactor: rename error and improve contract documentation

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -4,7 +4,13 @@ pragma solidity ^0.8.24;
 import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import "./PriceConverter.sol";
 
-error NotOwner();
+error FundMe__NotOwner();
+
+/** @title A contract for crowd funding
+ *  @author Priyanshu Desai
+ *  @notice This contract is to demo a sample funding contract
+ *  @dev This implements price feeds as our library
+ */
 
 contract FundMe {
     using PriceConvertor for uint256;
@@ -16,9 +22,22 @@ contract FundMe {
 
     AggregatorV3Interface public priceFeed;
 
+    modifier onlyOwner() {
+        if (msg.sender != i_owner) revert FundMe__NotOwner();
+        _;
+    }   
+
     constructor(address priceFeedAddress) {
         i_owner = msg.sender;
         priceFeed = AggregatorV3Interface(priceFeedAddress);
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
     }
 
     function fund() public payable {
@@ -48,18 +67,5 @@ contract FundMe {
         }
         funders = new address[](0);
         payable(msg.sender).transfer(address(this).balance);
-    }
-
-    modifier onlyOwner() {
-        if (msg.sender != i_owner) revert NotOwner();
-        _;
-    }
-
-    receive() external payable {
-        fund();
-    }
-
-    fallback() external payable {
-        fund();
     }
 }
